461e5aeb6557a885a078364af52f67620922a016
amontap <boat_111333@hotmail.com>
2014-10-22 16:06:45 +0700

    client server 512

---
+++ gen/buusk/example/client_saver_55410512/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package buusk.example.client_saver_55410512;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/buusk/example/client_saver_55410512/JParser.java
@@ -0,0 +1,5 @@
+package buusk.example.client_saver_55410512;
+
+public class JParser {
+
+}
